0daa24d54749544c205f27d192c89321f526cf70
ggerholm <ggerholm@kth.se>
2018-10-14 01:02:54 +0200

    Implemented merkle-trees.

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from merkle import MerkleTree
 
 tree = MerkleTree(16)
 
-f2= File(0, 'Det här är fil nr 1!')
-f4 = File(1, 'Det här är fil nr 2!')
+f1 = File(0, 'Det här är fil nr 1!')
+f2 = File(1, 'Det här är fil nr 2!')
 f3 = File(2, 'Det här är fil nr 3!')
-f1 = File(3, 'Det här är fil nr 4!')
+f4 = File(3, 'Det här är fil nr 4!')
 
 tree.add_file(f1)
 print(tree.top_node)
@@ -16,4 +16,3 @@ tree.add_file(f3)
 print(tree.top_node)
 tree.add_file(f4)
 print(tree.top_node)
-
